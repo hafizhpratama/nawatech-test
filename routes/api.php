3945cd9b8344451d00a9467881db2e3c5d5d1304
Hafizh Pratama <hafizh@alegrium.com>
2023-02-26 14:01:19 +0800

    Delete unused file.

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/bookings', [BookingController::class, 'index']);
 Route::get('confirm-email/{token}', [UserController::class, 'confirmEmail']);
 Route::post('register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
